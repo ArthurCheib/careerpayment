1c36ddad5105ce367e4d352304ec1c3b625ee087
Arthur Cheib <arthursilvacheib@gmail.com>
2018-09-27 16:46:39 -0300

    More balanced functions.

--- creating_dataset.R
+++ creating_dataset.R
@@ -2,17 +2,10 @@ library(tidyverse)
 library(readxl)
 library(data.table)
 
-# FIRST TRY ---------------------------------------------------------------
+# Creating the dataset by subsetting only the rows needed ------------------------------
 
-
-real_cols_names <- c("MASP", "NOME","SITUACAO_SERVIDOR", "CARGO_EFETIVO", "TEM_APOST", "CARGO_COMISSAO",
-                "ORGAO_ENTIDADE", "DESCUNID", "CARGA_HORARIA", "REM_BASICA_BRUTA",
-                "TETO", "JUDIC", "FERIAS","DECTER", "PREMIO", "FERIASPREM", "JETONS",
-                 "EVENTUAL", "IRRF", "CONT.PREVIDENCIRIA", "REM_POS_DEDUCOES", "ANO")
-
-empty_df <- data.frame()
+eppggs_df <- data.frame()
 all_files <- list.files(path="C:/Users/m7531338/OneDrive/Trabalho SEE/Datasets/pay_servidores", pattern = '.csv')
-
 
 for (i in seq_along(all_files)) {
   
@@ -20,11 +13,19 @@ for (i in seq_along(all_files)) {
           filter(V4 == "ESPEC.EM POLITICAS PUBLICAS E GESTAO GOVERNAMENTAL") %>% 
           mutate(year = all_files[i])
   
-  empty_df <- bind_rows(empty_df, x)
+  eppggs_df <- bind_rows(eppggs_df, x)
   
   rm(x)
   
 }
 
+# Inserting the column names and coercing the data type for numerical --------
+
+real_cols_names <- c("MASP", "NOME","SITUACAO_SERVIDOR", "CARGO_EFETIVO", "TEM_APOST", "CARGO_COMISSAO",
+                     "ORGAO_ENTIDADE", "DESCUNID", "CARGA_HORARIA", "REM_BASICA_BRUTA",
+                     "TETO", "JUDIC", "FERIAS","DECTER", "PREMIO", "FERIASPREM", "JETONS",
+                     "EVENTUAL", "IRRF", "CONT.PREVIDENCIRIA", "REM_POS_DEDUCOES", "ANO")
 colnames(empty_df) <- real_cols_names
 
+empty_df[c(1, 9:21)] <- map_df(empty_df[c(1, 9:21)], str_replace, "\\,", "\\.")
+empty_df[c(1, 9:21)] <- map_df(empty_df[c(1, 9:21)], as.numeric)
